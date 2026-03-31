b012ef21cc817edbfd711ac1a344cf1dfb95218d
minhle4397 <minh.le4397@gmail.com>
2026-03-31 21:32:56 +0700

    add function: penalty

--- src/catan_base.c
+++ src/catan_base.c
@@ -86,6 +86,31 @@ void action(char *action_sequence, char *inventory) {
             i++;
         }
     }
+    req['B'] = acts['R']*1 + acts['S']*1;
+    req['W'] = acts['R']*1 + acts['S']*1;
+    req['M'] = acts['S']*1 + acts['C']*2;
+    req['S'] = acts['S']*1 + acts['D']*1;
+    req['O'] = acts['C']*3 + acts['D']*1;
+
+    if(req['B']>inv['B'] || req['W']>inv['W'] || req['M']>inv['M']
+        || req['S']>inv['S'] || req['O']>inv['O']){
+            printf("Nuoc di khong hop le");
+            return;
+        }
+    
+    if(acts['C']>acts['S']){
+        printf("Nuoc di khong hop le");
+        return;
+    }
+
+    inv['B'] -= req['B'];
+    inv['W'] -= req['W'];
+    inv['M'] -= req['M'];
+    inv['S'] -= req['S'];
+    inv['O'] -= req['O'];
+
+    printf("%dW%dO%dM%dB%dS", inv['W'],inv['O'],inv['M'],
+    inv['B'],inv['S']);
     
 
     // ----- End: Student Answer -----
@@ -97,7 +122,24 @@ void action(char *action_sequence, char *inventory) {
 
 void penalty_7(char *inventory) {
     // ----- Begin: Student Answer -----
-    // students must implement this function
+    int sum = 0;
+    int i = 0;
+    int qty = 0;
+    while(inventory[i]!='\0'){
+        qty = 0;
+        while(isdigit(inventory[i])){
+            qty = qty*10 + (inventory[i]-'0');
+            i++;
+        }
+        sum += qty;
+        i++;
+    }
+    if(sum>=7){
+        if(sum%2==0)
+            printf("Ban can bo %d la tai nguyen. Siuuu\n", (sum/2));
+        else printf("Ban can bo %d la tai nguyen. Siuuu\n", (sum/2)+1);
+    }
+    else printf("Ban an toan\n");
 
     // ----- End: Student Answer -----
 }
--- src/test.c
+++ src/test.c
@@ -24,7 +24,7 @@ void action(char *action_sequence, char *inventory) {
             i++;
         }
     }
-    printf("%d\n",inv['W']);
+    
     i = 0;
     while(action_sequence[i]!='\0'){
         qty = 0;
@@ -38,14 +38,61 @@ void action(char *action_sequence, char *inventory) {
         }
     }
 
-    printf("%d",acts['R']);
+    req['B'] = acts['R']*1 + acts['S']*1;
+    req['W'] = acts['R']*1 + acts['S']*1;
+    req['M'] = acts['S']*1 + acts['C']*2;
+    req['S'] = acts['S']*1 + acts['D']*1;
+    req['O'] = acts['C']*3 + acts['D']*1;
+
+    if(req['B']>inv['B'] || req['W']>inv['W'] || req['M']>inv['M']
+        || req['S']>inv['S'] || req['O']>inv['O']){
+            printf("Nuoc di khong hop le\n");
+            return;
+        }
+    
+    if(acts['C']>acts['S']){
+        printf("Nuoc di khong hop le\n");
+        return;
+    }
+
+    inv['B'] -= req['B'];
+    inv['W'] -= req['W'];
+    inv['M'] -= req['M'];
+    inv['S'] -= req['S'];
+    inv['O'] -= req['O'];
+
+    printf("%dW%dO%dM%dB%dS\n", inv['W'],inv['O'],inv['M'],
+    inv['B'],inv['S']);
+    
+    // ----- End: Student Answer -----
+}
+void penalty_7(char *inventory) {
+    // ----- Begin: Student Answer -----
+    int sum = 0;
+    int i = 0;
+    int qty = 0;
+    while(inventory[i]!='\0'){
+        qty = 0;
+        while(isdigit(inventory[i])){
+            qty = qty*10 + (inventory[i]-'0');
+            i++;
+        }
+        sum += qty;
+        i++;
+    }
+    if(sum>=7){
+        if(sum%2==0)
+            printf("Ban can bo %d la tai nguyen. Siuuu\n", (sum/2));
+        else printf("Ban can bo %d la tai nguyen. Siuuu\n", (sum/2)+1);
+    }
+    else printf("Ban an toan\n");
+
     // ----- End: Student Answer -----
 }
 int main(){
-    char* s = "3B30W";
-    char* a = "27R";
-    int inv[256] = {0};
+    char* s = "1B1W";
+    char* a = "1S";
     action(a,s);
-    
+    penalty_7("1B1W1M");
     return 0;
 }
